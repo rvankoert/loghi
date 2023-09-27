6dc7d4d9abab729254df902793ef4179704f799b
Tim Koornstra <tim.koornstra@gmail.com>
2023-09-27 16:13:19 +0200

    Update Dockerfile and na-pipeline-train.sh for loghi-htr

--- na-pipeline-train.sh
+++ na-pipeline-train.sh
@@ -1,72 +1,73 @@
 #!/bin/bash
-# set to 1 if you want to enable, 0 otherwise, select just one
+
+# Configuration for HTR mode selection
 HTRLOGHI=1
 
+# Model configuration
 HTRLOGHIMODELHEIGHT=64
-
-HTRLOGHIMODEL=/src/loghi-htr-models/model-new12-generic_synthetic
-# HTRLOGHIMODEL=/tmp/tmp.eeJOqXEgFX/output/checkpoints/encoder12-saved-model-18-29.1521
-# maar!!!! zorg er voor dat alle karakters in de nieuwe set ook voorkomen in de bestaande.
-#--existing_model $HTRLOGHIMODEL
-
+HTRBASEMODEL=/src/loghi-htr-models/model-new12-generic_synthetic
 USEBASEMODEL=0
-#number of input channels to use
-channels=1
+
+# Define a VGSL model
+# This is equivalent to model10 in the model library
+HTRNEWMODEL="None,64,None,3 Cr3,3,24 Bn Mp2,2,2,2 Cr3,3,48 Bn Mp2,2,2,2 Cr3,3,96 Bn Cr3,3,96 Bn Mp2,2,2,2 Rc Bg256 Bg256 Bg256 Bg256 Bg256 O1s92"
+channels=3
 
 GPU=0
 
-#UPDATE THESE PATHS TO YOUR OWN FILES
-listdir=/scratch/randomprint2_lines
-trainlist=$listdir/training_all_train.txt
+# Dataset and training configuration
+listdir=/home/tim/Downloads/ijsberg-split/
+trainlist=$listdir/training_mini.txt
 validationlist=$listdir/training_all_val.txt
-#directory that contains the actual line strips
 datadir=/scratch/republicprint
-
 charlist=/data/htr_train_notdeeds/output_charlist.charlist
-
-epochs=20
+epochs=1
 height=$HTRLOGHIMODELHEIGHT
 multiply=1
-#minimum 2, maximum 5
-rnn_layers=5
-#minimum ??? maximum 1024?, bij kleine datasets wat lager.
-rnn_units=256
-batch_size=8
+batch_size=4
 model_name=myfirstmodel
-# tussen de 0.001 en 0.00001
-learning_rate=0.0001
-DECAYSTEPS=-1
+learning_rate=0.0003
 
-# DO NO EDIT BELOW THIS LINE
+# DO NOT EDIT BELOW THIS LINE
 tmpdir=$(mktemp -d)
-echo $tmpdir
 mkdir $tmpdir/output
 
-if [[ $USEBASEMODEL -eq 1 ]]
-then
-        BASEMODEL=" --existing_model "$HTRLOGHIMODEL
-        echo $BASEMODEL
+BASEMODEL=""
+if [[ $USEBASEMODEL -eq 1 ]]; then
+    BASEMODEL=" --existing_model "$HTRBASEMODEL
 fi
 
-# #HTR option 1 LoghiHTR
-if [[ $HTRLOGHI -eq 1 ]]
-then
-        echo "starting Loghi HTR"
-        echo docker run --gpus all --rm  -u $(id -u ${USER}):$(id -g ${USER}) -m 32000m --shm-size 10240m -ti -v /tmp:/tmp -v $tmpdir:$tmpdir -v $listdir:$listdir -v $datadir:$datadir loghi/docker.htr python3  /src/loghi-htr/src/main.py --do_train --train_list $trainlist --validation_list $validationlist --learning_rate $learning_rate --channels 4 --batch_size $batch_size --epochs $epochs --do_validate --gpu $GPU --height $height --use_mask --seed 1  --beam_width 10 --model new10 --rnn_layers $rnn_layers --rnn_units $rnn_units --use_gru --decay_steps 5000 --batch_normalization --multiply $multiply --output $listdir --model_name $model_name --output_charlist $tmpdir/output_charlist.charlist --output $tmpdir/output
-
-docker run --gpus all --rm  -u $(id -u ${USER}):$(id -g ${USER}) -m 32000m --shm-size 10240m -ti -v /tmp:/tmp -v $tmpdir:$tmpdir -v $listdir:$listdir -v $datadir:$datadir loghi/docker.htr python3 /src/loghi-htr/src/main.py --do_train --train_list $trainlist --validation_list $validationlist --learning_rate $learning_rate --channels 4 --batch_size $batch_size --epochs $epochs --do_validate --gpu $GPU --height $height --use_mask --seed 1  --beam_width 10 --model new10 --rnn_layers $rnn_layers --rnn_units $rnn_units --use_gru --decay_steps 5000 --batch_normalization --multiply $multiply --output $listdir --model_name $model_name --output_charlist $tmpdir/output_charlist.charlist --output $tmpdir/output
-
-
-# --replace_recurrent_layer \
-# --use_rnn_dropout \
-# --batch_normalization \
-# --dropout_rnn 0.5 \
-#--replace_final \
-
+# LoghiHTR option
+if [[ $HTRLOGHI -eq 1 ]]; then
+    echo "Starting Loghi HTR"
+    docker run --gpus all --rm  -u $(id -u ${USER}):$(id -g ${USER}) -m 32000m --shm-size 10240m -ti \
+        -v /tmp:/tmp \
+        -v $tmpdir:$tmpdir \
+        -v $listdir:$listdir \
+        -v $datadir:$datadir \
+        loghi/docker.htr python3 /src/loghi-htr/src/main.py \
+        --do_train \
+        --train_list $trainlist \
+        --do_validate \
+        --validation_list $validationlist \
+        --learning_rate $learning_rate \
+        --channels $channels \
+        --batch_size $batch_size \
+        --epochs $epochs \
+        --gpu $GPU \
+        --height $height \
+        --use_mask \
+        --seed 1 \
+        --beam_width 10 \
+        --model "$HTRNEWMODEL" \
+        --decay_steps 5000 \
+        --multiply $multiply \
+        --output $listdir \
+        --model_name $model_name \
+        --output_charlist $tmpdir/output_charlist.charlist \
+        --output $tmpdir/output
 fi
 
-echo "you can find the results here: "
+echo "Results can be found at:"
 echo $tmpdir
 
-# cleanup results
-# rm -rf $tmpdir
