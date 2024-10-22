57e2a1524d25769c4ed5eff4c6d207b52d16d0c4
Rutger van Koert <r.c.van.koert@gmail.com>
2024-10-22 13:51:59 +0200

    docker build

--- docker/docker.htr/buildImage.sh
+++ docker/docker.htr/buildImage.sh
@@ -30,9 +30,10 @@ cd $DIR_OF_SCRIPT
 #rm -rf ./$htr_folder/src/test15
 
 echo "Building docker image..."
-docker buildx build --platform linux/amd64 -t loghi/docker.htr .
+#docker build -t loghi/docker.htr .
+#docker buildx build --platform linux/amd64 -t loghi/docker.htr .
 #docker buildx build --platform linux/arm64 -t loghi/docker.htr .
-#docker build --no-cache . -t loghi/docker.htr
+docker build --no-cache . -t loghi/docker.htr
 
 
 #rm -rf ./$htr_folder
