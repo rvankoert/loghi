7f3e531a3809772110a740fb95f61c41c5c32044
Sunny Liu <sunnyyuchen@gmail.com>
2026-02-05 10:51:54 +0100

    update tab setting

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-extensions = ['myst_parser']
+extensions = ['myst_parser', 'sphinx_tabs.tabs']
 
 # -- Project information -----------------------------------------------------
 
